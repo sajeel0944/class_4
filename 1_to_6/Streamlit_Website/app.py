84ddec165a046cc95f99d54cbe68b6eb9a90a672
sajeek0944 <sajeel6y4z@gmail.com>
2025-03-25 15:43:22 +0500

    Project 4

--- 1_to_6/Streamlit_Website/app.py
+++ 1_to_6/Streamlit_Website/app.py
@@ -2,35 +2,38 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.write("Simple Data Dashboard")
+st.title("Simple Data Dashboard")
 
-up_load_flie = st.file_uploader("Choose CSV File", type="csv")
+# File uploader
+uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
 
-if up_load_flie is not None:
-    df = pd.read_csv(up_load_flie)
-
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    
+    # Data Preview
     st.subheader("Data Preview")
     st.write(df.head())
-
+    
+    # Data Summary
     st.subheader("Data Summary")
     st.write(df.describe())
-
-    st.subheader("Filter Data")
-    colums = df.columns.tolist()
-    selected_column = st.selectbox("selecte colum to filter by", colums)
-    unique_value = df[selected_column].unique()
-    seleted_value = st.selectbox("select value", unique_value)
-
-    filter_df = df[df[selected_column]  == seleted_value ]
-    st.write(filter_df)
-
-    st.subheader("Plot Data")
-    x_colmn = st.selectbox("select x_axis column ", colums)
-    y_colmn = st.selectbox("select y_axis column ", colums)
-
-    if st.button("Generate plot"):
-        st.line_chart(filter_df.set_index(x_colmn)[y_colmn])
-
-else:
-    st.write("waiting onfile upload...")
     
+    # Data Filtering
+    st.subheader("Filter Data")
+    columns = df.columns.tolist()
+    selected_column = st.selectbox("Select column to filter by", columns)
+    unique_values = df[selected_column].unique()
+    selected_value = st.selectbox("Select value", unique_values)
+    
+    filtered_df = df[df[selected_column] == selected_value]
+    st.write(filtered_df)
+    
+    # Data Plotting
+    st.subheader("Plot Data")
+    x_column = st.selectbox("Select X-axis column", columns)
+    y_column = st.selectbox("Select Y-axis column", columns)
+    
+    if st.button("Generate Plot"):
+        st.line_chart(filtered_df.set_index(x_column)[y_column])
+else:
+    st.warning("Waiting for file upload...")
