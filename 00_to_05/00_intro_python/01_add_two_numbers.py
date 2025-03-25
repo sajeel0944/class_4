8322fc858339c8bc45e665ca9d977991b9a8ce0e
sajeek0944 <sajeel6y4z@gmail.com>
2025-03-25 15:17:51 +0500

    Project 4

--- 00_to_05/00_intro_python/01_add_two_numbers.py
+++ 00_to_05/00_intro_python/01_add_two_numbers.py
@@ -1,41 +1,20 @@
-try:
-    frist_num : int = int(input("Enter your first number : "))
-    second_num : int = int(input("Enter your second number : "))
+def sum(num1:int, num2:int):
+
+    sum_number : int = num1 + num2
+    print(f"Answer : {sum_number}")
+
+def main():
+    frist_num : int = input("Enter your first number : ")
+    second_num : int = input("Enter your second number : ")
 
 
-    all_operator : list = ["+", "-", "X", "%", "÷"]
-    count : int = 1
-    for get_value in all_operator:
-        print(f"{count} : {get_value}")
-        count += 1
+    if frist_num == "" and second_num == "":
+        print("Please Enter Number")
+    else:
+        try:
+            sum(int(frist_num), int(second_num))
+        except ValueError as e:
+            print("Enter Only Number")
 
-    user_select_operator : int = int(input("select one option : "))
-    select_operator = all_operator[user_select_operator -1]
-
-
-    def main(num1:int, num2:int, operator:str):
-        print(f"\nyou select this option : ({operator})")
-
-        if operator == "+":
-            answer = num1 + num2
-            print(f"sum answer : {answer}")
-        elif operator == "-":
-            answer = num1 - num2
-            print(f"minus answer : {answer}")
-        elif operator == "X":
-            answer = num1 * num2
-            print(f"multiply answer : {answer}")
-        elif operator == "%":
-            answer = num1 % num2
-            print(f"modulus answer : {answer}")
-        elif operator == "÷":
-            answer = num1 / num2
-            print("division answer : {answer}")
-        else:
-            print("\nsome thing was wrong")
-
-    if __name__ == '__main__':
-        main(frist_num,second_num,select_operator)
-
-except  Exception as e:
-    print("\nplease enter the number")
+if __name__ in "__main__":
+    main()
